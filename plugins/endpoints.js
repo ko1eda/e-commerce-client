abc1477ab756b9e68b2993ffed5220aa973bc634
christopher-koleda <koleda.christopher@gmail.com>
2018-12-17 23:34:11 -0500

    Add endpoints plugin to list all endpoints for project in one place

---
+++ plugins/endpoints.js
@@ -0,0 +1,12 @@
+/**
+ * A consistent place to keep a list of all the endpoints used 
+ * for routing.
+ * https://nuxtjs.org/guide/plugins/#inject-into-context
+ */
+export default ({ app }, inject) => {
+  // Set the function directly on the context.app object
+  app.endpoints = {
+    categories: 'categories',
+    products: 'products'
+  };
+};
