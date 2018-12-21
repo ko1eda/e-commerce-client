6fb310f7cf2db5c562e91eb11e808bc694758fd6
christopher-koleda <koleda.christopher@gmail.com>
2018-12-20 21:46:19 -0500

    Add logic to resize thumbnail based on grid size in categories.index

--- components/Product/Product.vue
+++ components/Product/Product.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="tw-max-w-sm tw-overflow-hidden hover:tw-shadow-lg tw-shadow trans-shadow trans-overlay tw-cursor-pointer">
-    <div class="tw-w-full tw-relative">
-      <div class="overlay"/>
+  <div class="tw-max-w-sm tw-overflow-hidden hover:tw-shadow-lg tw-shadow trans-shadow trans-overlay tw-cursor-pointer tw-border tw-border-grey-light">
+    <div :class="['tw-w-full tw-relative max-w', height]">
+      <div class="overlay tw-absolute tw-w-full tw-h-full"/>
 
       <div class="tw-flex tw-justify-center tw-items-center tw-absolute tw-w-full tw-h-full">
         <p class="price tw-text-3xl tw-font-bold tw-text-white" v-text="'$' + product.price"/>
       </div>
       <!-- end overlay screen and price -->
-      <img :src="product.image_path" class="tw-inline-block tw-max-w-full">
+      <img :src="product.image_path" :class="['tw-inline-block tw-max-w-full tw-w-full', height]">
     </div>
-
+    <!-- end image section -->
     <div class="tw-px-4 tw-py-4 tw-leading-tight">
       <p class="tw-text-xl md:tw-text-2xl tw-font-light" v-text="product.name"/>
       <p class="tw-font-light tw-mt-1" v-text="product.description"/>
@@ -18,52 +18,112 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
   props: {
     product: {
       type: Object,
       required: true
     }
-  }
+  },
+
+  /**
+   * 
+   * height : this sets the min and max height correctly
+   *  for the image part of the card, no matter what the screen size is
+   * 
+   */
+  computed: {
+    height() {
+      let arr = [];
+      
+      if (this.gridSize() === 3) {
+        arr.push('min-h-400 max-h-400');
+      } 
+      else {
+        // if its size 4, the items are smaller
+        arr.push('min-h-300 max-h-300');
+      }
+
+      return arr;
+    }
+  },
+
+  methods: {
+    ...mapGetters({
+      gridSize: 'gridSize'
+    })
+  },
+
 };
 </script>
 
 <style lang="scss" scoped>
-.trans-shadow {
-  transition: all 1s cubic-bezier(0.215, 0.61, 0.355, 1);
-}
+  @import '~bulma/sass/utilities/mixins.sass';
 
-// This sets the opacity on the price
-// and the overlay itself when the parent class is hovered
-.trans-overlay {
-  &:hover {
-    .overlay {
-      opacity: 1;
-    }
+  .trans-shadow {
+    transition: all 1s cubic-bezier(0.215, 0.61, 0.355, 1);
+  }
 
-    div > .price {
-      opacity: 1;
+  .overlay {
+    z-index: 1;
+    background-color: config("colors.grey-dark");
+    top: 0px;
+    left: 0px;
+    opacity: 0;
+    transition: opacity .8s cubic-bezier(0.215, 0.61, 0.355, 1);
+  }
+
+  .price {
+    z-index: 2;
+    position: absolute;
+    opacity: 0;
+    transition: opacity .8s cubic-bezier(0.215, 0.61, 0.355, 1);
+  }
+
+  // This sets the opacity on the price
+  // and the overlay itself when the parent class is hovered
+  .trans-overlay {
+    &:hover {
+      .overlay {
+        opacity: .9;
+      }
+
+      div > .price {
+        opacity: 1;
+      }
     }
   }
-}
 
-.overlay {
-  z-index: 1;
-  width: 100%;
-  height: 99%;
-  position: absolute;
-  background-color: config("colors.grey-dark");
-  top: 0px;
-  left: 0px;
-  opacity: 0;
-  transition: opacity 0.6s cubic-bezier(0.215, 0.61, 0.355, 1);
-}
+  /**
+    * Used with height computer property
+    * These set the correct min and max height 
+    * regardless of screen size 
+  */
+  .min-h-300 {
+    min-height: 300px;
+  }
+  .min-h-400 {
+    min-height: 400px;
+    @include mobile {
+      min-height: 300px;
+    }
+  }
+  .max-h-300 {
+    max-height: 300px;
+  }
 
-.price {
-  z-index: 2;
-  position: absolute;
-  opacity: 0;
-  transition: opacity 0.6s cubic-bezier(0.215, 0.61, 0.355, 1);
-}
+  .max-h-400 {
+    max-height: 400px;
+    @include mobile {
+      max-height: 300px;
+    }
+  }
+  .max-w {
+   @include mobile {
+      max-width: 300px;
+    }
+  }
+  
 </style>
 
